063c1010fa943d688edbb82d0bc5f992c71553bc
selektoss <50732045+selektoss@users.noreply.github.com>
2019-11-19 21:47:54 +0300

    Update FuncArr.hpp

--- LR3/FuncArr.hpp
+++ LR3/FuncArr.hpp
@@ -1,6 +1,6 @@
+#include <iostream>
 #include "try_error.hpp"
-float * Great_Array_Dynamic_Memory(float*, int16_t &); // Создает массив.
-void Sort_Arr_Descending_InsertMethod(float*, const int16_t const &); // Сортирует массив
-void Show_Arr_Console(const float*, const int16_t const &); // Вывод на экран
-void Swap_Element_Arr(float*, const int16_t &); // Меняем местами элементы
-float* Delete_Arr(float*); // Удаляем массив
+float * Great_Array_Dynamic_Memory(float*, int16_t &); 
+void Sort_Arr_Descending_InsertMethod(float*, const int16_t const &); 
+void Show_Arr_Console(const float*, const int16_t const &);
+float* Delete_Arr(float*);
