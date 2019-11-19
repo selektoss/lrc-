3c992cdaac054187aacf80f1e124c2dfc24eadfa
selektoss <50732045+selektoss@users.noreply.github.com>
2019-11-19 22:18:42 +0300

    Update FuncArray.hpp

--- LR4/FuncArray.hpp
+++ LR4/FuncArray.hpp
@@ -4,4 +4,4 @@ float** Great_Array_Dynamic_Memory(float**, int16_t&, int16_t&);
 float** Delete_Arr(float**);
 void Show_Arr_Console(float **, const int16_t&, const int16_t&);
 void Search_ColumnArr_SumElement_Positive(float **, const int16_t&, const int16_t&);
-void Ñyclic_Shift_Arr(float**, const int16_t&);
+void Cyclic_Shift_Arr(float**, const int16_t&);
