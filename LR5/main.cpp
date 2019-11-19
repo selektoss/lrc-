f384e3d3afe9dfd8d5eaa71b5ccdc9b4b09e96fa
selektoss <50732045+selektoss@users.noreply.github.com>
2019-11-19 22:48:22 +0300

    Update main.cpp

--- LR5/main.cpp
+++ LR5/main.cpp
@@ -1,24 +1,19 @@
-#include <stdio.h>
 #include "FuncFile.hpp"
 
 int main(void)
 {
 	printf_s("%s", "0. Enter name txt file.  \r\n1. Open file to process.\
 				  \r\n2. Enter the number K to search for sentences consisting of a given number of words.\
-				  \r\n3. Close File.\
-				  \r\n4. Exit program.");
-	size_t ChoiceNumberMenu = NULL;
-	const size_t MaxChar = 21;
-	char fileName[MaxChar];
+				  \r\n3. Exit program.");
+	size_t ChoiceNumberMenu = NULL; const size_t MaxChar = 21; char fileName[MaxChar]; char* ArrText = nullptr;
 	FILE* fileX = nullptr;
-	char* ArrText = nullptr;
 
 	do
 	{
 		printf("%s", "\r\nMake your choice: ");
 		Try_Error_Scan(ChoiceNumberMenu);
 
-		if ((ChoiceNumberMenu <= 4) && (ChoiceNumberMenu >= NULL)) {
+		if ((ChoiceNumberMenu <= 3) && (ChoiceNumberMenu >= NULL)) {
 			switch (ChoiceNumberMenu)
 			{
 			case 0:
@@ -31,15 +26,11 @@ int main(void)
 				ArrText = SearchSuggestionsAndGreateDynamicMemory(fileX, fileName, ArrText);
 				break;
 			case 3:
-				fileX = CloseFile(fileX);
-				break;
-			case 4:
 				break;
 			}
-		}
-		else printf("%s", "Error. Repeat your choice  Waiting : ");
-	} while (ChoiceNumberMenu != 4);
-	
+		} else printf("%s", "Error. Repeat your choice  Waiting : ");
+
+	} while (ChoiceNumberMenu != 3);
 	
 	return 0;
 }
