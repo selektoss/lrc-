34db59e27d09df0da5f7a58198b3ab8e17438aa8
selektoss <50732045+selektoss@users.noreply.github.com>
2019-11-19 22:20:45 +0300

    Update try_error_cin.cpp

--- LR4/try_error_cin.cpp
+++ LR4/try_error_cin.cpp
@@ -1,9 +1,7 @@
-#include <string>
-#include <iostream>
-std::string RepeatMessageWait = "Repeat: ";
+#include "try_error_cin.hpp"
 const int16_t MaxError = 5;
 
-void Try_Error_Cin(int16_t &ChoiceNumber)
+void Try_Error_Cin(int16_t& ChoiceNumber)
 {
 	int CountError = 0;
 	while (!(std::cin >> ChoiceNumber))
@@ -15,16 +13,17 @@ void Try_Error_Cin(int16_t &ChoiceNumber)
 			std::cin.ignore(INT_MAX, '\n');
 			break;
 		}
-		std::cout << "Invalid value!" << std::endl << RepeatMessageWait;
+		std::cout << "Invalid value!" << std::endl << "Repeat: ";
 		std::cin.clear();
 		std::cin.ignore(INT_MAX, '\n');
 	}
-}
+};
+
 float Try_Error_Cin(float& ElementNumberInsertArray)
 {
 	while (!(std::cin >> ElementNumberInsertArray))
 	{
-		std::cout << "Invalid value!" << std::endl << RepeatMessageWait;
+		std::cout << "Invalid value!" << std::endl << "Repeat: ";
 		std::cin.clear();
 		std::cin.ignore(INT_MAX, '\n');
 	}
@@ -37,12 +36,16 @@ bool Try_Error_Cin(char& ChoiceYesOrNo)
 	{
 		std::cin >> ChoiceYesOrNo;
 		if (ChoiceYesOrNo == 'y' || ChoiceYesOrNo == 'n')
+		{
+			std::cin.clear();
+			std::cin.ignore(INT_MAX, '\n');
 			return (ChoiceYesOrNo == 'y');
+		}
 		else
 		{
-			std::cout << "Invalid character" << std::endl << RepeatMessageWait;
+			std::cout << "Invalid character" << std::endl << "Repeat: ";
 			std::cin.clear();
 			std::cin.ignore(INT_MAX, '\n');
 		}
 	}
-}
+};
