245fcc91dd40c4c301066b010b082fedbfe7b410
selektoss <50732045+selektoss@users.noreply.github.com>
2019-11-21 22:00:32 +0300

    Update FuncArr.hpp

--- LR3/FuncArr.hpp
+++ LR3/FuncArr.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include "try_error.hpp"
-float * Great_Array_Dynamic_Memory(float*, int16_t &); 
-void Sort_Arr_Descending_InsertMethod(float*, const int16_t const &); 
-void Show_Arr_Console(const float*, const int16_t const &);
+float * Great_Array_Dynamic_Memory(float*, uint16_t &); 
+void Sort_Arr_Descending_InsertMethod(float*, const uint16_t const &); 
+void Show_Arr_Console(const float*, const uint16_t const &);
 float* Delete_Arr(float*);
