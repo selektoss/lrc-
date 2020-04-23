2dbe728514756bd6d84944f8fea5517a03126d95
selektoss <50732045+selektoss@users.noreply.github.com>
2020-04-23 23:35:08 +0300

    Add files via upload

--- KP/GetDataResource.cpp
+++ KP/GetDataResource.cpp
@@ -11,10 +11,10 @@
 void GetDataResourceMenu(ModelOBJ*& form, char*& title_contetnt, char**& nameSection)
 {
 	using namespace std;
-	const char* regular[] = {	"#HEADER\r","^(#\\END_HEADER[\\s\\S]*)", "#MENU\r", "^(#\\END_MENU[\\s\\S]*)",
-								"#HELP\r", "^(#\\END_HELP[\\s\\S]*)", "#TITLE\r", "^(#\\END_TITLE[\\s\\S]*)", 
-								"#CONTENT\r", "^(#\\END_CONTENT[\\s\\S]*)", "^(#\\END_CONF[\\s\\S]*)"
-							};
+	const char* regular[] = { "#HEADER\r","^(#\\END_HEADER[\\s\\S]*)", "#MENU\r", "^(#\\END_MENU[\\s\\S]*)",
+		"#HELP\r", "^(#\\END_HELP[\\s\\S]*)", "#TITLE\r", "^(#\\END_TITLE[\\s\\S]*)",
+		"#CONTENT\r", "^(#\\END_CONTENT[\\s\\S]*)", "^(#\\END_CONF[\\s\\S]*)"
+	};
 
 	string lineTemp, lineTemp1; stringstream lineBuffM(F::app | F::ate | F::in | F::out); std::stringstream lineBuff;
 	uint16_t check(NULL); uint16_t count(NULL); cmatch res; form->Menu = nullptr;
@@ -26,10 +26,14 @@ void GetDataResourceMenu(ModelOBJ*& form, char*& title_contetnt, char**& nameSec
 		if (lineBuff.good())
 			while (getline(lineBuff, lineTemp) && !(regex_match(lineTemp.c_str(), regex{ *(regular + 10) })))
 			{
-				if (regex_match(lineTemp.c_str(), res, regex{ "^(#\\HEADER[\\s\\S]|#\\MENU[\\s\\S]|#\\HELP[\\s\\S]|#\\TITLE[\\s\\S]|#\\CONTENT[\\s\\S]*)" }, std::regex_constants::match_not_eol))
+				if (regex_match(lineTemp.c_str(), res, 
+					regex{ "^(#\\HEADER[\\s\\S]|#\\MENU[\\s\\S]|#\\HELP[\\s\\S]|#\\TITLE[\\s\\S]|#\\CONTENT[\\s\\S]*)" }, 
+					std::regex_constants::match_not_eol))
 				{
-					res[0] == regular[0] ? check = 1 : res[0] == regular[2] ? check = 3 : res[0] == regular[4] ? check = 5 : res[0] == regular[8] ? check = 9 : check = 7;
-					while (getline(lineBuff, lineTemp) && !(regex_match(lineTemp.c_str(), regex{ *(regular + check) })))
+					res[0] == regular[0] ? check = 1 : res[0] == regular[2] ? check = 3 : 
+						res[0] == regular[4] ? check = 5 : res[0] == regular[8] ? check = 9 : check = 7;
+					while (getline(lineBuff, lineTemp) && !(regex_match(lineTemp.c_str(), 
+						regex{ *(regular + check) })))
 					{
 						if (lineTemp == "\r" || lineTemp == "") continue;
 						if (check == 1)
@@ -51,7 +55,8 @@ void GetDataResourceMenu(ModelOBJ*& form, char*& title_contetnt, char**& nameSec
 					{
 						form->sizeMenu = count;
 						for (uint16_t iter(0); iter < count; ++iter)
-							check == 3 ? getline(lineBuffM, ((form->Menu) + iter)->nameButton) : getline(lineBuffM, ((form->Menu) + iter)->txtHelp);
+							check == 3 ? getline(lineBuffM, ((form->Menu) + iter)->nameButton) : 
+							getline(lineBuffM, ((form->Menu) + iter)->txtHelp);
 						lineBuffM.str(std::string()); lineBuffM.clear();
 					}
 					else if (check == 9)
@@ -75,7 +80,8 @@ void GetDataResourceMenu(ModelOBJ*& form, char*& title_contetnt, char**& nameSec
 		else
 		{
 			const char* info_error = "Error load resource! Check resource ID. Exit programm";
-			SetCurPos(static_cast<uint16_t>((form->coordXY.X >> 1) - ((strlen(info_error)) >> 1)), static_cast<uint16_t>(form->coordXY.Y >> 1));
+			SetCurPos(static_cast<uint16_t>((form->coordXY.X >> 1) - ((strlen(info_error)) >> 1)), 
+				static_cast<uint16_t>(form->coordXY.Y >> 1));
 			std::cout << info_error;
 			Sleep(2000); exit(-2);
 		}
